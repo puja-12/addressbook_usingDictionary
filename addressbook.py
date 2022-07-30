e68a1f6bfe74a590a1b0497bd341ca05bd9f8be0
puja <pujarana1796@gmail.com>
2022-07-30 15:03:49 +0530

    puja added addressbook

--- addressbook.py
+++ addressbook.py
@@ -7,6 +7,9 @@ class Contact:
         self.email = email
         self.phone = phone_number
 
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
 
 class Addressbook:
 
@@ -15,55 +18,85 @@ class Addressbook:
         # {contact1.first_name:contact1,contact2.first_name:contact2}
 
     def add_contacts(self, contact_object):
-        self._people_dict.update({contact_object.first_name: contact_object})
-        self._people_dict.update({contact_object.last_name: contact_object})
-        self._people_dict.update({contact_object.address: contact_object})
-        self._people_dict.update({contact_object.email: contact_object})
-        self._people_dict.update({contact_object.phone: contact_object})
+
+        num = int(input("Please enter initial number of contacts: "))
+
+        for i in range(num):
+            print("Enter the First name :")
+            contact_object.first_name = input()
+            print("Enter the Last name :")
+            contact_object.last_name = input()
+            print("Enter the Address :")
+            contact_object.address = input()
+            print("Enter the Email :")
+            contact_object.email = input()
+            print("Enter the Phone Number :")
+            contact_object.phone = input()
+            self._people_dict.update({contact_object.first_name: contact_object})
 
     def all_values(self):
         # Iterate over all values of the dictionary
         for key, value in self._people_dict.items():
-            print(key, value)
+            print(key, f"{value.full_name()},{value.address},{value.email},{value.phone}")
 
     def edit(self, contact_object):
-        self._people_dict[contact_object.first_name] = 'Richa'
-        self._people_dict[contact_object.last_name] = 'Mishra'
-        self._people_dict[contact_object.address] = 'UP'
-        self._people_dict[contact_object.email] = 'dscnsj@123'
-        self._people_dict[contact_object.phone] = '567894390'
-        print(self._people_dict)
 
-    def delete(self, contact_object):
-        del self._people_dict[contact_object.first_name]
-        del self._people_dict[contact_object.last_name]
-        del self._people_dict[contact_object.address]
-        del self._people_dict[contact_object.email]
-        del self._people_dict[contact_object.phone]
-        print("Deleted the contact.")
+        key = input("Enter key to check:")
+        if key in self._people_dict:
+            print("given name contact exists")
+            print(
+                "choose the option to change the data : \n1) firstName\n2)lastName\n3)address\n4)Email\n5)Phone "
+                "Number")
+            choice = int(input())
+            if choice == 1:
+                print("Please enter the first name : ")
+                first_name = input()
+                contact_object.first_name = first_name
+            elif choice == 2:
+                print("Please enter the last name : ")
+                last_name = input()
+                contact_object.last_name = last_name
+            elif choice == 3:
+                print("Please enter the Address : ")
+                address = input()
+                contact_object.address = address
+            elif choice == 4:
+                print("Please enter the email : ")
+                email = input()
+                contact_object.email = email
+            elif choice == 5:
+                print("Please enter the Phone Number : ")
+                phone = input()
+                contact_object.phone = phone
+            else:
+                print("please choose from above options :")
+
+            print("updated successfully :")
+
+    def RemoveContact(self):
+        key = input("Enter key to check:")
+        if key in self._people_dict:
+            print("given name contact exists")
+            self._people_dict.pop(key)
+
+            print("contact deleted successfully")
+        else:
+            print("Key isn't present!")
 
 
 if __name__ == "__main__":
     address_book = Addressbook()
-    contact1 = Contact(f_name="Pooja", l_name="Rana", address="Delhi", email="pooja@123", phone_number="786869543")
-    contact2 = Contact(f_name="Rahul", l_name="Sharma", address="Meerut", email="rahul@123", phone_number="123456789")
-
+    contact1 = Contact('f_name', 'l_name', 'address', 'email', 'phone_number')
     while True:
         print("Enter the option : \n1)Add Contact \n2)Display contact\n3)update contact\n4)RemoveContact")
         option = int(input())
         if option == 1:
             address_book.add_contacts(contact1)
-            address_book.add_contacts(contact2)
-            print(address_book._people_dict)
-            # x = address_book._people_dict.get("xyz")
-            # y = address_book._people_dict.get("dhhdf")
-            # print(y.phone)
-            # print(x.phone)
         elif option == 2:
             address_book.all_values()
         elif option == 3:
-            address_book.edit(contact2)
+            address_book.edit(contact1)
         elif option == 4:
-            address_book.delete(contact2)
+            address_book.RemoveContact()
         else:
             print("Please choose correct option")
