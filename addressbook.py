a057e56f9e386362a4845678a76db5c2b8fe3365
puja <pujarana1796@gmail.com>
2022-08-01 13:33:45 +0530

    puja added addressbook

--- addressbook.py
+++ addressbook.py
@@ -10,6 +10,9 @@ class Contact:
     def full_name(self):
         return self.first_name + " " + self.last_name
 
+    def details(self):
+        return f"{self.full_name()},{self.address},{self.email},{self.phone}"
+
 
 class Addressbook:
 
@@ -17,13 +20,80 @@ class Addressbook:
         self._people_dict = {}
         # {contact1.first_name:contact1,contact2.first_name:contact2}
 
-    def add_contacts(self):
+    def add_contacts(self, contact_object):
+        self._people_dict.update({contact_object.first_name: contact_object})
 
-        num = int(input("Please enter initial number of contacts: "))
+    def all_values(self):
+        # Iterate over all values of the dictionary
+        for key, value in self._people_dict.items():
+            print(key, value.details())
 
-        for i in range(num):
+    def edit(self, first_name, last_name, address, email, phone):
+        contact = self.get_contact(first_name)
+        if not contact:
+            print("key is not present")
+            return
+
+        contact.last_name = last_name
+        contact.address = address
+        contact.email = email
+        contact.phone = phone
+        print("updated successfully")
+
+        # key = input("Enter key to check:")
+        # if key in self._people_dict:
+        #     update= self._people_dict.get()
+        #     print("given name contact exists")
+        #     print(
+        #         "choose the option to change the data : \n1) firstName\n2)lastName\n3)address\n4)Email\n5)Phone "
+        #         "Number")
+        #     choice = int(input())
+        #     if choice == 1:
+        #         print("Please enter the first name : ")
+        #         first_name = input()
+        #         update.first_name = first_name
+        #     elif choice == 2:
+        #         print("Please enter the last name : ")
+        #         last_name = input()
+        #         update.last_name = last_name
+        #     elif choice == 3:
+        #         print("Please enter the Address : ")
+        #         address = input()
+        #         update.address = address
+        #     elif choice == 4:
+        #         print("Please enter the email : ")
+        #         email = input()
+        #         update.email = email
+        #     elif choice == 5:
+        #         print("Please enter the Phone Number : ")
+        #         phone = input()
+        #         update.phone = phone
+        #     else:
+        #         print("please choose from above options :")
+        #
+        #     print("updated successfully :")
+
+    def get_contact(self, first_name):
+        return self._people_dict.get(first_name)
+
+    def remove_contact(self, first_name):
+        contact = self.get_contact(first_name)
+        if not contact:
+            print("given name not exists")
+            return
+        self._people_dict.pop(first_name)
+
+        print("contact deleted successfully")
+
+
+if __name__ == "__main__":
+    address_book = Addressbook()
+
+    while True:
+        print("Enter the option : \n1)Add Contact \n2)Display contact\n3)update contact\n4)RemoveContact")
+        option = int(input())
+        if option == 1:
             contact1 = Contact('f_name', 'l_name', 'address', 'email', 'phone_number')
-
             print("Enter the First name :")
             contact1.first_name = input()
             print("Enter the Last name :")
@@ -34,72 +104,24 @@ class Addressbook:
             contact1.email = input()
             print("Enter the Phone Number :")
             contact1.phone = input()
-            self._people_dict.update({contact1.first_name: contact1})
-
-    def all_values(self):
-        # Iterate over all values of the dictionary
-        for key, value in self._people_dict.items():
-            print(key, f"{value.full_name()},{value.address},{value.email},{value.phone}")
-
-    def edit(self):
-
-        key = input("Enter key to check:")
-        if key in self._people_dict:
-            update= self._people_dict.get(key)
-            print("given name contact exists")
-            print(
-                "choose the option to change the data : \n1) firstName\n2)lastName\n3)address\n4)Email\n5)Phone "
-                "Number")
-            choice = int(input())
-            if choice == 1:
-                print("Please enter the first name : ")
-                first_name = input()
-                update.first_name = first_name
-            elif choice == 2:
-                print("Please enter the last name : ")
-                last_name = input()
-                update.last_name = last_name
-            elif choice == 3:
-                print("Please enter the Address : ")
-                address = input()
-                update.address = address
-            elif choice == 4:
-                print("Please enter the email : ")
-                email = input()
-                update.email = email
-            elif choice == 5:
-                print("Please enter the Phone Number : ")
-                phone = input()
-                update.phone = phone
-            else:
-                print("please choose from above options :")
-
-            print("updated successfully :")
-
-    def remove_contact(self):
-        key = input("Enter key to check:")
-        if key in self._people_dict:
-            print("given name contact exists")
-            self._people_dict.pop(key)
-
-            print("contact deleted successfully")
-        else:
-            print("Key isn't present!")
-
-
-if __name__ == "__main__":
-    address_book = Addressbook()
-
-    while True:
-        print("Enter the option : \n1)Add Contact \n2)Display contact\n3)update contact\n4)RemoveContact")
-        option = int(input())
-        if option == 1:
-            address_book.add_contacts()
+            address_book.add_contacts(contact1)
         elif option == 2:
             address_book.all_values()
         elif option == 3:
-            address_book.edit()
+            key = input("Enter key to check:")
+            print("given name contact exists")
+            print("Please enter the last name : ")
+            last_name = input()
+            print("Please enter the Address : ")
+            address = input()
+            print("Please enter the email : ")
+            email = input()
+            print("Please enter the Phone Number : ")
+            phone = input()
+            address_book.edit(key, last_name, address, email, phone)
         elif option == 4:
-            address_book.remove_contact()
+            key = input("Enter key to check:")
+            address_book.remove_contact(key)
+
         else:
             print("Please choose correct option")
