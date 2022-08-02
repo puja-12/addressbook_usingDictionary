14602dfa898a8fd0b6cb1bb3b36f5913ee56e594
puja <pujarana1796@gmail.com>
2022-08-02 12:17:31 +0530

    puja added addressbook

--- addressbook.py
+++ addressbook.py
@@ -15,18 +15,32 @@ class Contact:
 
 
 class Addressbook:
+    """
+    creating a class in which we define functions for addressbook
+    """
 
     def __init__(self):
         self._people_dict = {}
-        # {contact1.first_name:contact1,contact2.first_name:contact2}
+
+    """
+    method to create addressbook
+    """
 
     def add_contacts(self, contact_object):
         self._people_dict.update({contact_object.first_name: contact_object})
+
+    """
+    method to read all values from addressbook
+    """
 
     def all_values(self):
         # Iterate over all values of the dictionary
         for key, value in self._people_dict.items():
             print(key, value.details())
+
+    """
+    method to edit existing data in addressbook
+    """
 
     def edit(self, first_name, last_name, address, email, phone):
         contact = self.get_contact(first_name)
@@ -43,6 +57,10 @@ class Addressbook:
     def get_contact(self, first_name):
         return self._people_dict.get(first_name)
 
+    """
+    method to remove contact from the addressbook
+    """
+
     def remove_contact(self, first_name):
         contact = self.get_contact(first_name)
         if not contact:
@@ -56,39 +74,74 @@ class Addressbook:
 if __name__ == "__main__":
     address_book = Addressbook()
 
+    """
+    using dictionary mapping
+    """
+
+
+    def choice1():
+        print("Enter the First name :")
+        first_name = input()
+        print("Enter the Last name :")
+        last_name = input()
+        print("Enter the Address :")
+        address = input()
+        print("Enter the Email :")
+        email = input()
+        print("Enter the Phone Number :")
+        phone = input()
+        contact1 = Contact(first_name, last_name, address, email, phone)
+        address_book.add_contacts(contact1)
+
+
+    def choice2():
+        address_book.all_values()
+
+
+    def choice3():
+        key = input("Enter key to check:")
+        print("given name contact exists")
+        print("Please enter the last name : ")
+        last_name = input()
+        print("Please enter the Address : ")
+        address = input()
+        print("Please enter the email : ")
+        email = input()
+        print("Please enter the Phone Number : ")
+        phone = input()
+        address_book.edit(key, last_name, address, email, phone)
+
+
+    def choice4():
+        key = input("Enter key to check:")
+        address_book.remove_contact(key)
+
+
+    def default():
+        print("Please choose correct option")
+
+
+    switcher = {
+        1: choice1,
+        2: choice2,
+        3: choice3,
+        4: choice4,
+
+    }
     while True:
         print("Enter the option : \n1)Add Contact \n2)Display contact\n3)update contact\n4)RemoveContact")
         option = int(input())
         if option == 1:
-            contact1 = Contact('f_name', 'l_name', 'address', 'email', 'phone_number')
-            print("Enter the First name :")
-            contact1.first_name = input()
-            print("Enter the Last name :")
-            contact1.last_name = input()
-            print("Enter the Address :")
-            contact1.address = input()
-            print("Enter the Email :")
-            contact1.email = input()
-            print("Enter the Phone Number :")
-            contact1.phone = input()
-            address_book.add_contacts(contact1)
+            switcher.get(option)()
+
         elif option == 2:
-            address_book.all_values()
+            switcher.get(option)()
+
         elif option == 3:
-            key = input("Enter key to check:")
-            print("given name contact exists")
-            print("Please enter the last name : ")
-            last_name = input()
-            print("Please enter the Address : ")
-            address = input()
-            print("Please enter the email : ")
-            email = input()
-            print("Please enter the Phone Number : ")
-            phone = input()
-            address_book.edit(key, last_name, address, email, phone)
+            switcher.get(option)()
+
         elif option == 4:
-            key = input("Enter key to check:")
-            address_book.remove_contact(key)
+            switcher.get(option)()
 
         else:
-            print("Please choose correct option")
+            default()
